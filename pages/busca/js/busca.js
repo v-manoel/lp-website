f6df536bc4f7aded9c80e6a63aff0d8290e23622
Vitor M <victororeide2009@gmail.com>
2021-04-19 22:38:13 -0300

    Criada nova search page / Finalizada paginas de pagamento / device lock construido para as paginas de gerenciamento

--- pages/busca/js/busca.js
+++ pages/busca/js/busca.js
@@ -7,3 +7,23 @@ $(function () {
                   ' Text is: ' + this.options[this.selectedIndex].textContent);
     });
   });
+
+  
+/* Sidebar */
+$(document).ready(function () {
+  $("#sidebar").mCustomScrollbar({
+      theme: "minimal"
+  });
+
+  $('#dismiss, .overlay').on('click', function () {
+      $('#sidebar').removeClass('active');
+      $('.overlay').removeClass('active');
+  });
+
+  $('#sidebarCollapse').on('click', function () {
+      $('#sidebar').addClass('active');
+      $('.overlay').addClass('active');
+      $('.collapse.in').toggleClass('in');
+      $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+  });
+});
