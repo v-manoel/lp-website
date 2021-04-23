3db8f51f3022c6443e3cdf2d897643ad1d92a0f5
Vitor M <victororeide2009@gmail.com>
2021-04-23 19:52:56 -0300

    Criado painel do administrador e tablet dos setores

---
+++ pages/admin/js/client-graphs.js
@@ -0,0 +1,237 @@
+'use strict';
+
+/* Chart.js docs: https://www.chartjs.org/ */
+
+window.chartColors = {
+	green: '#75c181',
+	gray: '#a9b5c9',
+	text: '#252930',
+	border: '#e7e9ed'
+};
+
+/* Random number generator for demo purpose */
+var randomDataPoint = function(){ return Math.round(Math.random()*10000)};
+
+
+//Chart.js Line Chart Example 
+
+var morePurchaseGraph = {
+	type: 'line',
+
+	data: {
+		labels: ['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6', 'Day 7'],
+		
+		datasets: [{
+			label: 'Client 1',
+			fill: false,
+			backgroundColor: window.chartColors.green,
+			borderColor: window.chartColors.green,
+			data: [
+				randomDataPoint(),
+				randomDataPoint(),
+				randomDataPoint(),
+				randomDataPoint(),
+				randomDataPoint(),
+				randomDataPoint(),
+				randomDataPoint()
+			],
+		}, {
+			label: 'Client 2',
+			backgroundColor: window.chartColors.gray,
+			borderColor: window.chartColors.gray,
+			
+			data: [
+				randomDataPoint(),
+				randomDataPoint(),
+				randomDataPoint(),
+				randomDataPoint(),
+				randomDataPoint(),
+				randomDataPoint(),
+				randomDataPoint()
+			],
+			fill: false,
+		}, {
+			label: 'Client 3',
+			backgroundColor: "#ffc107",
+			borderColor: "#ffc107",
+			
+			data: [
+				randomDataPoint(),
+				randomDataPoint(),
+				randomDataPoint(),
+				randomDataPoint(),
+				randomDataPoint(),
+				randomDataPoint(),
+				randomDataPoint()
+			],
+			fill: false,
+		}]
+	},
+	options: {
+		responsive: true,	
+		aspectRatio: 1.5,
+		
+		legend: {
+			display: true,
+			position: 'bottom',
+			align: 'end',
+		},
+		
+		title: {
+			display: true,
+			text: 'Chart.js Line Chart Example',
+			
+		}, 
+		tooltips: {
+			mode: 'index',
+			intersect: false,
+			titleMarginBottom: 10,
+			bodySpacing: 10,
+			xPadding: 16,
+			yPadding: 16,
+			borderColor: window.chartColors.border,
+			borderWidth: 1,
+			backgroundColor: '#fff',
+			bodyFontColor: window.chartColors.text,
+			titleFontColor: window.chartColors.text,
+
+            callbacks: {
+	            //Ref: https://stackoverflow.com/questions/38800226/chart-js-add-commas-to-tooltip-and-y-axis
+                label: function(tooltipItem, data) {
+	                if (parseInt(tooltipItem.value) >= 1000) {
+                        return "$" + tooltipItem.value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                    } else {
+	                    return '$' + tooltipItem.value;
+                    }
+                }
+            },
+
+		},
+		hover: {
+			mode: 'nearest',
+			intersect: true
+		},
+		scales: {
+			xAxes: [{
+				display: true,
+				gridLines: {
+					drawBorder: false,
+					color: window.chartColors.border,
+				},
+				scaleLabel: {
+					display: false,
+				
+				}
+			}],
+			yAxes: [{
+				display: true,
+				gridLines: {
+					drawBorder: false,
+					color: window.chartColors.border,
+				},
+				scaleLabel: {
+					display: false,
+				},
+				ticks: {
+		            beginAtZero: true,
+		            userCallback: function(value, index, values) {
+		                return '$' + value.toLocaleString();   //Ref: https://stackoverflow.com/questions/38800226/chart-js-add-commas-to-tooltip-and-y-axis
+		            }
+		        },
+			}]
+		}
+	}
+};
+
+
+
+// Chart.js Bar Chart Example 
+
+var regionGraph = {
+	type: 'bar',
+
+	data: {
+		labels: ['bairro', 'bairro', 'bairro', 'bairro', 'bairro', 'bairro', 'bairro'],
+		datasets: [{
+			label: 'Bairros',
+			backgroundColor: "#ffc107",
+			borderColor: "#ffc107",
+			borderWidth: 1,
+			maxBarThickness: 20,
+			
+			data: [
+				23,
+				45,
+				76,
+				75,
+				62,
+				37,
+				83
+			]
+		}]
+	},
+	options: {
+		responsive: true,
+		aspectRatio: 1.5,
+		legend: {
+			position: 'bottom',
+			align: 'end',
+		},
+		title: {
+
+		},
+		tooltips: {
+			mode: 'index',
+			intersect: false,
+			titleMarginBottom: 10,
+			bodySpacing: 10,
+			xPadding: 16,
+			yPadding: 16,
+			borderColor: window.chartColors.border,
+			borderWidth: 1,
+			backgroundColor: '#fff',
+			bodyFontColor: window.chartColors.text,
+			titleFontColor: window.chartColors.text,
+
+		},
+		scales: {
+			xAxes: [{
+				display: true,
+				gridLines: {
+					drawBorder: false,
+					color: window.chartColors.border,
+				},
+
+			}],
+			yAxes: [{
+				display: true,
+				gridLines: {
+					drawBorder: false,
+					color: window.chartColors.borders,
+				},
+
+				
+			}]
+		}
+		
+	}
+}
+
+
+
+
+
+
+
+// Generate charts on load
+window.addEventListener('load', function(){
+	
+	var regionbar = document.getElementById('region-bar').getContext('2d');
+	window.myBar = new Chart(regionbar, regionGraph);
+
+	var morepurchase = document.getElementById('morepurchase-bar').getContext('2d');
+	window.myLine = new Chart(morepurchase, morePurchaseGraph);
+	
+
+});	
+	
---
+++ pages/admin/js/product-graphs.js
@@ -0,0 +1,313 @@
+'use strict';
+
+/* Chart.js docs: https://www.chartjs.org/ */
+
+window.chartColors = {
+	green: '#75c181', // rgba(117,193,129, 1)
+	blue: '#5b99ea', // rgba(91,153,234, 1)
+	gray: '#a9b5c9',
+    secondary: '#666',
+    yellow: '#ffc107',
+    white: '#ffff',
+	text: '#252930',
+	border: '#e7e9ed'
+};
+
+/* Random number generator for demo purpose */
+var randomDataPoint = function(){ return Math.round(Math.random()*100)};
+
+// Doughnut Chart Demo
+
+
+var doughnutChartConfig = {
+	type: 'doughnut',
+	data: {
+		datasets: [{
+			data: [
+				randomDataPoint(),
+				randomDataPoint(),
+				randomDataPoint(),
+                randomDataPoint(),
+			],
+			backgroundColor: [
+				window.chartColors.green,
+				window.chartColors.yellow,
+				window.chartColors.gray,
+                window.chartColors.white,
+
+			],
+			label: 'Dataset 1'
+		}],
+		labels: [
+			'First',
+			'Second',
+			'Third',
+            'Others',
+		]
+	},
+	options: {
+		responsive: true,
+		legend: {
+			display: true,
+			position: 'bottom',
+			align: 'center',
+		},
+
+		tooltips: {
+			titleMarginBottom: 10,
+			bodySpacing: 10,
+			xPadding: 16,
+			yPadding: 16,
+			borderColor: window.chartColors.border,
+			borderWidth: 1,
+			backgroundColor: '#fff',
+			bodyFontColor: window.chartColors.text,
+			titleFontColor: window.chartColors.text,
+			
+			animation: {
+				animateScale: true,
+				animateRotate: true
+			},
+			
+			/* Display % in tooltip - https://stackoverflow.com/questions/37257034/chart-js-2-0-doughnut-tooltip-percentages */
+			callbacks: {
+                label: function(tooltipItem, data) {
+					//get the concerned dataset
+					var dataset = data.datasets[tooltipItem.datasetIndex];
+					//calculate the total of this data set
+					var total = dataset.data.reduce(function(previousValue, currentValue, currentIndex, array) {
+					return previousValue + currentValue;
+					});
+					//get the current items value
+					var currentValue = dataset.data[tooltipItem.index];
+					//calculate the precentage based on the total and current item, also this does a rough rounding to give a whole number
+					var percentage = Math.floor(((currentValue/total) * 100)+0.5);
+					
+					return percentage + "%";
+			    },
+            },
+			
+
+		},
+	}
+};
+
+var morePurchaseGraph = {
+	type: 'line',
+
+	data: {
+		labels: ['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6', 'Day 7'],
+		
+		datasets: [{
+			label: 'Product 1',
+			fill: false,
+			backgroundColor: window.chartColors.green,
+			borderColor: window.chartColors.green,
+			data: [
+				randomDataPoint(),
+				randomDataPoint(),
+				randomDataPoint(),
+				randomDataPoint(),
+				randomDataPoint(),
+				randomDataPoint(),
+				randomDataPoint()
+			],
+		}, {
+			label: 'Product 2',
+			backgroundColor: window.chartColors.gray,
+			borderColor: window.chartColors.gray,
+			
+			data: [
+				randomDataPoint(),
+				randomDataPoint(),
+				randomDataPoint(),
+				randomDataPoint(),
+				randomDataPoint(),
+				randomDataPoint(),
+				randomDataPoint()
+			],
+			fill: false,
+		}, {
+			label: 'Product 3',
+			backgroundColor: "#ffc107",
+			borderColor: "#ffc107",
+			
+			data: [
+				randomDataPoint(),
+				randomDataPoint(),
+				randomDataPoint(),
+				randomDataPoint(),
+				randomDataPoint(),
+				randomDataPoint(),
+				randomDataPoint()
+			],
+			fill: false,
+		}]
+	},
+	options: {
+		responsive: true,	
+		aspectRatio: 1.5,
+		
+		legend: {
+			display: true,
+			position: 'bottom',
+			align: 'end',
+		},
+		
+		title: {
+			
+		}, 
+		tooltips: {
+			mode: 'index',
+			intersect: false,
+			titleMarginBottom: 10,
+			bodySpacing: 10,
+			xPadding: 16,
+			yPadding: 16,
+			borderColor: window.chartColors.border,
+			borderWidth: 1,
+			backgroundColor: '#fff',
+			bodyFontColor: window.chartColors.text,
+			titleFontColor: window.chartColors.text,
+
+            callbacks: {
+	            //Ref: https://stackoverflow.com/questions/38800226/chart-js-add-commas-to-tooltip-and-y-axis
+                label: function(tooltipItem, data) {
+	                if (parseInt(tooltipItem.value) >= 1000) {
+                        return "$" + tooltipItem.value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                    } else {
+	                    return '$' + tooltipItem.value;
+                    }
+                }
+            },
+
+		},
+		hover: {
+			mode: 'nearest',
+			intersect: true
+		},
+		scales: {
+			xAxes: [{
+				display: true,
+				gridLines: {
+					drawBorder: false,
+					color: window.chartColors.border,
+				},
+				scaleLabel: {
+					display: false,
+				
+				}
+			}],
+			yAxes: [{
+				display: true,
+				gridLines: {
+					drawBorder: false,
+					color: window.chartColors.border,
+				},
+				scaleLabel: {
+					display: false,
+				},
+				ticks: {
+		            beginAtZero: true,
+		            userCallback: function(value, index, values) {
+		                return '$' + value.toLocaleString();   //Ref: https://stackoverflow.com/questions/38800226/chart-js-add-commas-to-tooltip-and-y-axis
+		            }
+		        },
+			}]
+		}
+	}
+};
+
+
+// Chart.js Bar Chart Example 
+
+var prodGraph = {
+	type: 'bar',
+
+	data: {
+		labels: ['prod1', 'prod2', 'prod3', 'prod4', 'prod5', 'prod6', 'prod7', 'prod8', 'prod9', 'prod10', 'prod11', 'prod12'],
+		datasets: [{
+			label: 'Bairros',
+			backgroundColor: "#ffc107",
+			borderColor: "#ffc107",
+			borderWidth: 1,
+			maxBarThickness: 20,
+			
+			data: [
+				23,
+				45,
+				76,
+				75,
+				62,
+				37,
+				83,
+                12,
+                102,
+                20,
+                24,
+                52
+			]
+		}]
+	},
+	options: {
+		responsive: true,
+		aspectRatio: 1.5,
+		legend: {
+			position: 'bottom',
+			align: 'end',
+		},
+		title: {
+
+		},
+		tooltips: {
+			mode: 'index',
+			intersect: false,
+			titleMarginBottom: 10,
+			bodySpacing: 10,
+			xPadding: 16,
+			yPadding: 16,
+			borderColor: window.chartColors.border,
+			borderWidth: 1,
+			backgroundColor: '#fff',
+			bodyFontColor: window.chartColors.text,
+			titleFontColor: window.chartColors.text,
+
+		},
+		scales: {
+			xAxes: [{
+				display: true,
+				gridLines: {
+					drawBorder: false,
+					color: window.chartColors.border,
+				},
+
+			}],
+			yAxes: [{
+				display: true,
+				gridLines: {
+					drawBorder: false,
+					color: window.chartColors.borders,
+				},
+
+				
+			}]
+		}
+		
+	}
+}
+
+
+// Generate charts on load
+window.addEventListener('load', function(){
+	
+    var prodbar = document.getElementById('prod-bar').getContext('2d');
+	window.myBar = new Chart(prodbar, prodGraph);
+
+	var productDonut = document.getElementById('product-donut').getContext('2d');
+	window.myDoughnut = new Chart(productDonut, doughnutChartConfig);
+
+    var morepurchase = document.getElementById('morepurchase-bar').getContext('2d');
+	window.myLine = new Chart(morepurchase, morePurchaseGraph);
+
+});	
+	
